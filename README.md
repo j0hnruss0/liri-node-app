77c99dbc4dae7fc97c12eb0dd065f0ae3850eccb
Johnny <johnrussoofficial@outlook.com>
2019-06-29 18:14:30 -0400

    project complete, data saving functionality added

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ An easy to use CLI search tool for finding your favorite songs on Spotify, looki
 
 Node Packages used: Node Spotify API, Axios, Dotenv, Moment
 
+*Updates*: 
+* Search queries no longer have to be contained within quotes
+* all search results are now appended to a log.txt file, as well as being displayed in the command line
+
 In the command prompt, type in "node liri.js" then one of the following commands:
 * "spotify-this-song 'song name'": Search for the top ten tracks off the Spotify API that match that name
 * "concert-this 'band name'": Find the most recent concerts around the world headlining your favorite bands
 * "movie-this 'movie name'": Get some trivia about movies you want to learn more about. Not too wordy!
 * "do-what-it-says": make a command with a search term as dictated by the random.txt file. If you change what random.txt says, the output changes, too!. Just be sure to keep that file in the right format (no spaces, command followed by comma followed by search term in double quotes(""))
-* *Helpful Reminder*: Put your search queries in quotes, especially if your movie/band/song name is more than one word!
 
 ### Look for upcoming concerts with "concert-this" command
 [Demo1](LIRI-Bot_Demo.gif)
--- liri.js
+++ liri.js
@@ -6,10 +6,18 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 
+//NOTICE: package.json may show 'inquirer' as a dependency but it is not being used
+// in the current version of this app. There are no additional imputs besides
+// 'concert-this', 'movie-this', and 'spotify-this-song', followed by a search term or name
+
 var nodeArgs = process.argv;
 
 var spotify = new Spotify(keys.spotify);
 
+// This function searches for concerts throught the Bands in Town API according
+// to the band name entered after 'concert-this' in the terminal. The ten most
+// recent show times (coverted to a readavle format with moment.js) are displayed
+// as well as appended to the log.txt file
 var concertThis = function() {
 
   var BandsInTownURL = "https://rest.bandsintown.com/artists/" + searchParam + "/events?app_id=codingbootcamp";
@@ -17,11 +25,17 @@ var concertThis = function() {
   axios.get(BandsInTownURL).then(
     function(response) {
       for (var i = 0; i < 10; i++) {
-        console.log("Band(s) playing: " + response.data[i].lineup.join(", "));
-        console.log("Concert location: " + response.data[i].venue.name);
-        console.log(response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
-        console.log(moment(response.data[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a"));
-        console.log("---------------------------");
+        var concertItem = "Band(s) playing: " + response.data[i].lineup.join(", ") + "\nConcert location: " + response.data[i].venue.name
+        + "\n" + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country
+        + "\n" + moment(response.data[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a") + "\n------------------------------\n";
+        
+        console.log(concertItem);
+        
+        fs.appendFile("log.txt", concertItem, function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        });
       }
       
     })
@@ -47,21 +61,27 @@ var concertThis = function() {
     });
 }
 
+// The movieThis function allows the user to type 'movie-this' in the CL, followed
+// by a movie title, to show information related to that movie in the terminal and
+// then append all that info to the log.txt file
 var movieThis = function() {
 
   var OmdbURL = "https://www.omdbapi.com/?t=" + searchParam + "&apikey=trilogy";
 
   axios.get(OmdbURL).then(
     function(movieResponse) {
-      console.log(movieResponse.data.Title);
-      console.log(movieResponse.data.Year);
-      console.log("IMDb Rating: " + movieResponse.data.Ratings[0].Value);
-      console.log("Rotten Tomatoes Rating: " + movieResponse.data.Ratings[1].Value);
-      console.log("Produced in " + movieResponse.data.Country);
-      console.log(movieResponse.data.Language);
-      console.log(movieResponse.data.Plot);
-      console.log(movieResponse.data.Actors);
-      console.log("---------------------------------------");
+      var movieItem = movieResponse.data.Title + "\n" + movieResponse.data.Year
+      + "\nIMDb Rating: " + movieResponse.data.Ratings[0].Value + "\nRotten Tomatoes Rating: " + movieResponse.data.Ratings[1].Value
+      + "\nProduced in " + movieResponse.data.Country + "\n" + movieResponse.data.Language
+      + "\n" + movieResponse.data.Plot + "\n" + movieResponse.data.Actors + "\n---------------------------------------\n"
+      
+      console.log(movieItem);
+      
+      fs.appendFile("log.txt", movieItem, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
       
     })
     .catch(function(error) {
@@ -85,34 +105,54 @@ var movieThis = function() {
 
 }
 
+//This function gets the top 5 songs from Node Spotify API, shows them in the console then
+//appends the list to the log.txt file
 var spotifyThisSong = function() {
   spotify
   .search({ type: 'track', query: searchParam })
   .then(function(response) {
-    for (var i = 10; i > 0; i--) {
-      console.log("Song: " + response.tracks.items[i].name);
-      console.log("Performed by: " + response.tracks.items[i].artists[0].name);
-      console.log("Album: " + response.tracks.items[i].album.name);
-      console.log("Spotify Page for this Artist: " + response.tracks.items[i].artists[0].external_urls.spotify)
-      console.log("Preview: " + response.tracks.items[i].preview_url);
-      console.log("---------------------------------------");
-    }
+      for (var i = 0; i < 5; i++) {
+        if (JSON.stringify(response.tracks.items[i].name).toLowerCase().includes(searchParam)) {
+          var item = "Song: " + response.tracks.items[i].name + "\nPerformed by: " + response.tracks.items[i].artists[0].name 
+            + "\nAlbum: " + response.tracks.items[i].album.name + "\nSpotify Page for this Artist: " + response.tracks.items[i].artists[0].external_urls.spotify
+            + "\nPreview: " + response.tracks.items[i].preview_url + "\n---------------------------------------" + "\n";
+
+          console.log(item);
+          
+          fs.appendFile("log.txt", item, function(err) {
+            if (err) {
+              return console.log(err);
+            }
+          });
+          
+        }
+      }
+
   })
   .catch(function(err) {
     console.log(err);
   });
 }
 
+// var addLog = function(command, query) {
+//   var item = "[" + command + ", " + query + "]\n";
+//   fs.appendFile("log.txt", item, function(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//   });
+// }
+
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 //FUNCTION CALLS HERE
 
 if (nodeArgs[2] === "concert-this") {
   if (nodeArgs.length >= 3) {
-    var searchParam = nodeArgs[3];
+    var searchParam = nodeArgs.slice(3).join(" ");
     concertThis(searchParam);
   }
 } else if (nodeArgs[2] === "movie-this") {
-  var searchParam = nodeArgs[3];
+  var searchParam = nodeArgs.slice(3).join(" ");
   if (nodeArgs[3] !== undefined) {
     movieThis(searchParam);
   } else {
@@ -120,20 +160,26 @@ if (nodeArgs[2] === "concert-this") {
     movieThis(searchParam);
   }
 } else if (nodeArgs[2] === "spotify-this-song") {
-  var searchParam = nodeArgs[3];
+  var searchParam = nodeArgs.slice(3).join(" ");
   if (nodeArgs[3] !== undefined) {
     spotifyThisSong(searchParam);
   } else {
     spotify
     .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
     .then(function(data) {
-      //console.log(data);
-      console.log("Song: " + data.name);
-      console.log("Performed by: " + data.artists[0].name);
-      console.log("Album: " + data.album.name);
-      console.log("Spotify Page for this Artist: " + data.artists[0].external_urls.spotify)
-      console.log("Preview: " + data.preview_url);
-      console.log("---------------------------------------"); 
+      
+      var aobItem = "Song: " + data.name + "\nPerformed by: " + data.artists[0].name 
+            + "\nAlbum: " + data.artists[0].name + "\nSpotify Page for this Artist: " + data.artists[0].external_urls.spotify
+            + "\nPreview: " + data.preview_url + "\n---------------------------------------" + "\n";
+
+      console.log(aobItem);
+          
+      fs.appendFile("log.txt", aobItem, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+      
     })
     .catch(function(err) {
       console.error('Error occurred: ' + err); 
